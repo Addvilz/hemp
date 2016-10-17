5d97f7b1cea38e8bb3d1f2dee71510172ad16e4d
Matiss Treinis <mrtreinis@gmail.com>
2016-10-17 18:39:57 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Hemp',
-    version="0.0.3",
+    version="0.0.4",
     description='Tools for Fabric',
     long_description="Tools for Fabric",
     author='Addvilz',
